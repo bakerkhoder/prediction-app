2a3d4cc2c0056ae1cd77a6315ca154f0ff0fd802
Baker Khoder <bakerkhoder878@gmail.com>
2022-09-09 18:27:05 +0300

    showing a dog image in every upload

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
-//gender : https://api.genderize.io/?name=rio
-//:agge:  https://api.agify.io/?name=nour
-//:nationality: https://api.nationalize.io/?name=mohamad
-//dog image: https://dog.ceo/api/breeds/image/random
+/*showing a dog image in every render */
+const imageapi="https://dog.ceo/api/breeds/image/random"
+let image=document.querySelector("img")
+fetch(imageapi)
+	.then(response => response.json())
+	.then((data) => {
+        console.log(data.message)
+        image.src=data.message
+    })
+	.catch(err => console.error(err));  
+
 btn=document.querySelector("button")
 btn.addEventListener("click",()=>{
 let name =document.querySelector("input").value
@@ -11,6 +18,7 @@ let nationality=document.querySelector(".nationality")
 const genderapi="https://api.genderize.io/?name=rio".replace("rio", name);
 const ageapi="https://api.agify.io/?name=nour".replace("nour", name);
 const nationalityapi="https://api.nationalize.io/?name=mohamad".replace("mohamad", name);
+
 
 /*fetching data for the gender and putting them in their tags */
 fetch(genderapi)
@@ -39,9 +47,14 @@ fetch(nationalityapi)
             nationality.textContent+=item.country_id+"    "
         })
         })
-    .catch(err => console.error(err));    
+    .catch(err => console.error(err));
+    
+    
+     
+
    })
-	    
+
+   
 
 
 
