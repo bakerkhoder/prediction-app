424769045a5a6c48f712fce99feff79462e977d5
Baker Khoder <bakerkhoder878@gmail.com>
2022-09-09 17:54:58 +0300

    retrieving the age  of the name

--- index.js
+++ index.js
@@ -7,6 +7,10 @@ btn.addEventListener("click",()=>{
 let name =document.querySelector("input").value
 let gender=document.querySelector(".gender")
 const genderapi="https://api.genderize.io/?name=rio".replace("rio", name);
+const ageapi="https://api.agify.io/?name=nour".replace("rio", name);
+const nationalityapi="https://api.nationalize.io/?name=mohamad".replace("rio", name);
+
+/*fetching data for the gender and putting them in their tags */
 fetch(genderapi)
 	.then(response => response.json())
 	.then((data) => {
@@ -14,7 +18,25 @@ fetch(genderapi)
     gender.textContent=data.gender})
 	.catch(err => console.error(err));
 
-//gender.textContent=dta    
+/*fetching data for the age and putting them in their tags */
+fetch(ageapi)
+	.then(response => response.json())
+	.then((data) => {
+        console.log(data)
+    gender.textContent=data.age})
+	.catch(err => console.error(err));
+
+
+/*fetching data for the age and putting them in their tags */
+fetch(nationalityapi)
+	.then(response => response.json())
+	.then((data) => {
+        console.log(data)
+    gender.textContent=data.nationality})
+	.catch(err => console.error(err));    
+
+
+
 })
 
 
