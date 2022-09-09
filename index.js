2bd6a7c6b57c06dea78db762bf21234fc4b4f3be
Baker Khoder <bakerkhoder878@gmail.com>
2022-09-09 18:15:40 +0300

    retrieving the nationalities from api

--- index.js
+++ index.js
@@ -6,9 +6,11 @@ btn=document.querySelector("button")
 btn.addEventListener("click",()=>{
 let name =document.querySelector("input").value
 let gender=document.querySelector(".gender")
+let age=document.querySelector(".age")
+let nationality=document.querySelector(".nationality")
 const genderapi="https://api.genderize.io/?name=rio".replace("rio", name);
-const ageapi="https://api.agify.io/?name=nour".replace("rio", name);
-const nationalityapi="https://api.nationalize.io/?name=mohamad".replace("rio", name);
+const ageapi="https://api.agify.io/?name=nour".replace("nour", name);
+const nationalityapi="https://api.nationalize.io/?name=mohamad".replace("mohamad", name);
 
 /*fetching data for the gender and putting them in their tags */
 fetch(genderapi)
@@ -23,21 +25,27 @@ fetch(ageapi)
 	.then(response => response.json())
 	.then((data) => {
         console.log(data)
-    gender.textContent=data.age})
+    age.textContent=data.age})
 	.catch(err => console.error(err));
 
 
-/*fetching data for the age and putting them in their tags */
+/*fetching data for the nationality and putting them in their tags */
 fetch(nationalityapi)
 	.then(response => response.json())
 	.then((data) => {
-        console.log(data)
-    gender.textContent=data.nationality})
-	.catch(err => console.error(err));    
+        console.log(data.country)
+        let countries=data.country
+        countries.forEach((item)=>{
+            nationality.textContent+=item.country_id+"    "
+        })
+        })
+    .catch(err => console.error(err));    
+   })
+	    
 
 
 
-})
+
 
 
 document.querySelector(".prediction").classList.add("rred")
